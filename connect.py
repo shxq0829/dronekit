3efea47fe7cb20aece252bf84cbadf1274db9a0f
shxq0829 <shxq0829@163.com>
2016-07-02 16:58:35 +0800

    d

--- connect.py
+++ connect.py
@@ -1,3 +1,23 @@
-from dronekit import connect
+import dronekit
+import socket
+import exceptions
 
-vehicle = connect('127.0.0.1:5760', wait_ready=True)
+
+try:
+    dronekit.connect('127.0.0.1:5760', heartbeat_timeout=15)
+
+# Bad TCP connection
+except socket.error:
+    print 'No server exists!'
+
+# Bad TTY connection
+except exceptions.OSError as e:
+    print 'No serial exists!'
+
+# API Error
+except dronekit.APIException:
+    print 'Timeout!'
+
+# Other error
+except:
+    print 'Some other error!'
